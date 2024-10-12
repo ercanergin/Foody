ceb7a595ef28af6b69d9105f6b8fdacf621da329
ercanergin <ercanergin623@gmail.com>
2024-10-12 21:38:17 +0300

    View Components On The Layout Page

---
+++ Foody.PresentationLayer/Models/ViewComponents/AdminLayoutViewComponents/_MainPanelLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Models.ViewComponents.AdminLayoutViewComponents
+{
+    public class _MainPanelLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoike()
+        {
+            return View();
+        }
+    }
+}
